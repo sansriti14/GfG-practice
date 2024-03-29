17a3e98de27deb3cc1f56a074a55b806c1934ace
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-03-29 18:58:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Euler Circuit in an Undirected Graph/euler-circuit-in-an-undirected-graph.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	bool isEularCircuitExist(int v, vector<int>adj[]){
+	    vector<int> indegree (v, 0);
+        for (int i = 0; i < v; i++) {
+            for(auto it: adj[i]) {
+                indegree[it]++;
+            }
+        }
+        
+        for (int i = 0; i < v; i++) {
+            if(indegree[i] % 2) {
+                return false;
+            }
+        }
+        
+        return true;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isEularCircuitExist(V, adj);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
