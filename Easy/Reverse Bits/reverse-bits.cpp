116913dab1ebe8cee5814b81ec99ce0391dff301
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-04-13 01:24:49 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Bits/reverse-bits.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        long long ans = 0;
+        for (int i = 0; i < 32; i++) {
+            long long  p = ((x >> i) & 1);
+            ans |= (p << (31 - i));
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
